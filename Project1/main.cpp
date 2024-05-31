5c30feee43ae4ec34e063d2d6f2fd7e9c964e24c
pitteam11 <p3trogoncharov@yandex.ru>
2024-05-31 19:13:42 +0300

    first struct

--- Project1/main.cpp
+++ Project1/main.cpp
@@ -2,7 +2,23 @@
 
 using namespace std;
 
+struct Point {
+	double x;
+	double y;
+	string color;
+};
+
+void test_point() {
+	Point p1;
+	p1.x = 13;
+	p1.y = 19;
+	p1.color = "red";
+
+	cout << p1.x << " " << p1.y << " " << p1.color << endl;
+}
+
 void main() {
 	cout << "Hello git" << endl;
 	cout << "Bye git" << endl;
+	test_point();
 }
