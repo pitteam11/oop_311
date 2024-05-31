b073bdb87230b49f31e07e4dd9ef647dc87d2594
pitteam11 <p3trogoncharov@yandex.ru>
2024-05-31 19:38:41 +0300

    struct array

--- Project1/main.cpp
+++ Project1/main.cpp
@@ -3,18 +3,38 @@
 using namespace std;
 
 struct Point {
-	double x;
-	double y;
-	string color;
+	double x = 0;
+	double y = 0;
+	double z = 0;
+	string color = "white";
 };
 
-void test_point() {
-	Point p1;
+void show_point(const Point& pt) {
+	cout << "(" << pt.x << ", "
+		 << pt.y << ", "
+		 << pt.z << ") - "
+		 << pt.color << endl;
+}
+
+void test_point() {	
+	Point p1, p2;
 	p1.x = 13;
 	p1.y = 19;
+	p1.z = 7;
 	p1.color = "red";
 
-	cout << p1.x << " " << p1.y << " " << p1.color << endl;
+	show_point(p1);
+	show_point(p2);
+
+	Point pnts[10];
+	pnts[0] = {12, 14, 14, "black"};
+	
+	for (int k = 1; k < 10; k++)
+		pnts[k] = { (double)k, 12, 0, "blue" };
+
+	for (int k = 1; k < 10; k++)
+		show_point(pnts[k]);
+
 }
 
 void main() {
