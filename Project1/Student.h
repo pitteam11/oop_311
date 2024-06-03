5d4d17ca5b79251d7ee4bfc1758bd4509253ddd3
pitteam11 <p3trogoncharov@yandex.ru>
2024-06-03 21:25:35 +0300

    students_vs_university

--- Project1/Student.h
+++ Project1/Student.h
@@ -4,21 +4,64 @@
 using namespace std;
 
 class Student {
+
+	enum UNIVERS {MSU, SFU, DSTU, MIT};
+
+	class Univercity {
+	private:
+		UNIVERS name;
+		string city;
+		string country;
+
+	public:
+		Univercity(UNIVERS name) {
+			switch (name)
+			{
+			case Student::MSU:
+				city = "Moscow";
+				country = "Russia";
+				break;
+			case Student::SFU:				
+			case Student::DSTU:
+				city = "Rostov-on-Don";
+				country = "Russia";
+				break;
+			case Student::MIT:
+				city = "Cambridge";
+				country = "USA";
+				break;
+			default:
+				break;
+			}
+		}
+	};
+
 private:
-	int id;
-	int year;
-	string name;
+	string fio;
 	int age;
+	string phone;
+	string city;
+	string country;
+	//Univercity univercity;
 
 public:
-	void show() {
-		cout << name << " " << age << endl;
+	Student(){
+		//univercity = Univercity(UNIVERS::MIT);
 	}
 
-	Student() {
-		id = 1;
-		name = "Vasya";
-		age = 20;
+	void set_phone(string phone) {
+		if (phone.size() == 10) {
+			if (phone[0] == '7')
+				this->phone = "+" + phone;
+			else if (phone[0] == '8') {
+				phone[0] = '7';
+				this->phone = "+" + phone;
+			}
+		}
+	}
+	
+	void show() {
+		cout << fio << " " << phone << endl;
 	}
 
 	~Student(){
