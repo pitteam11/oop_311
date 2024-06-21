47fb30d58bd2e3fda38ff90c540003d0c911d587
pitteam11 <p3trogoncharov@yandex.ru>
2024-06-21 21:21:54 +0300

    simply class Number finall

--- Project8/Source.cpp
+++ Project8/Source.cpp
@@ -5,8 +5,8 @@ using namespace std;
 class Number {
 private:
 	int n;
-public:
 
+public:
 	explicit Number(const int n=0) : n{ n } {
 	}
 
@@ -14,7 +14,7 @@ public:
 		return n;
 	}
 
-	inline void set_n(const int n) {
+	inline void set_n(const int n=0) {
 		this->n = n;
 	}
 
@@ -29,33 +29,32 @@ public:
 	}
 
 	// arithmetical
-	Number& operator+(const Number& b) const {
-		auto res = Number(n + b.n);
+	Number operator+(const Number& obj) const {
+		auto res = Number(n + obj.n);
 		return res;
 	}
 
-	Number& operator*(const Number& b) const {
-		auto res = Number(n * b.n);
+	Number operator*(const Number& obj) const {
+		auto res = Number(n * obj.n);
 		return res;
 	}
 
-	Number& operator-(const Number& b) const {
-		auto res = Number(b.n - n);
+	Number operator-(const Number& obj) const {
+		auto res = Number(n - obj.n);
 		return res;
 	}
 
-	Number& operator/(const Number& b) const {
-		auto res = Number(b.n / n);
+	Number operator/(const Number& obj) const {
+		auto res = Number(n / obj.n);
 		return res;
 	}
 
-	Number& operator%(const Number& b) const {
-		auto res = Number(n%b.n);
+	Number operator%(const Number& obj) const {
+		auto res = Number(n % obj.n);
 		return res;
 	}
 
-
-	// logical
+	// logical - like operator C++20 <=> "spaceship"
 	bool operator==(const Number& b) const {		
 		return n == b.n;
 	}
@@ -80,16 +79,55 @@ public:
 		return n != b.n;
 	}
 
+	// copy constructor (перегрузка не нужна)
+	Number(const Number& obj) {
+		n = obj.n;		
+	}
+
+	// Запретить автоматическое создание оператора копирования
+	//Number operator=(const Number& obj) = delete;
+
+	// Заставить создать по-умолчанию оператор копирования
+	//Number operator=(const Number& obj) = default;
+	
+	// appropriation - copy operator (перегрузка не нужна)
+	Number operator=(const Number& obj) {
+		auto res = Number(obj.n);
+		return res;
+	}
+};
+
+class Empty {
+
+public:
+	//~Empty() = delete;
 };
 
 void test1() {
 	Number a(123);
+	//a.set_n(512);
+	int r = a.get_n();
+	cout << ++r << endl;
+
 	Number b(7);
 	Number c = a + b;
+	Number d(8);	
+	Number rr = a;
+	Number z;
+	z = b;
+
 	cout << a << " + " << b << " = " << c << endl;
+	cout << a << " - " << b << " = " << a-b << endl;
+	cout << a << " / " << b << " = " << a/b << endl;
+	cout << a << " % " << b << " = " << a%b << endl;
+	cout << a + b + d << endl;
 }
 
 int main() {
-	test1();
+
+	Empty r();
+
+
+	//test1();
 	return 0;
 }
