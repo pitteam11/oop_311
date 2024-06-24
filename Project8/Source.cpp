954e255197eb73aaf5fb67f3bd3e3b4ddf9e2482
pitteam11 <p3trogoncharov@yandex.ru>
2024-06-24 21:28:22 +0300

    move constuctor

--- Project8/Source.cpp
+++ Project8/Source.cpp
@@ -91,10 +91,27 @@ public:
 	//Number operator=(const Number& obj) = default;
 	
 	// appropriation - copy operator (перегрузка не нужна)
-	Number operator=(const Number& obj) {
+	Number& operator=(const Number& obj) {
 		auto res = Number(obj.n);
 		return res;
 	}
+
+	Number& operator=(Number&& obj) {
+		auto res = Number(obj.n);
+		return res;
+	}
+
+	
+
+	Number add(int val) {
+		n += val;
+		return *this;
+	}
+
+	Number mult(int val) {
+		n *= val;
+		return *this;
+	}	
 };
 
 class Empty {
@@ -121,13 +138,126 @@ void test1() {
 	cout << a << " / " << b << " = " << a/b << endl;
 	cout << a << " % " << b << " = " << a%b << endl;
 	cout << a + b + d << endl;
+
+	Number new_a = a.add(15).mult(2).add(-5);
+	z = new_a; // тут не работает
+	cout << z << endl;
 }
 
+void test_reference() {
+	int very_long_name_for_variables = 17;
+	int x = very_long_name_for_variables;
+	// l-value reference
+	int& y = very_long_name_for_variables;
+	y = 2;
+	cout << very_long_name_for_variables << endl;
+
+	int&& rval = 4 + 17;	
+	cout << rval << endl;
+}
+
+class DynArray {
+private:
+	int* data;
+	int size = 0;
+
+public:
+	DynArray(int size, int* data) : size{size} {
+		// deep copy
+		this->data = new int[size];
+		for (int k = 0; k < size; k++)
+			this->data[k] = data[k];
+	}
+
+	// copy constructor
+	DynArray(const DynArray& a) : size{a.size} {
+		// deep copy
+		if (this != &a) {
+			this->data = new int[size];
+			for (int k = 0; k < size; k++)
+				this->data[k] = a.data[k];
+		}
+	}
+
+	// move constructor
+	DynArray(DynArray&& a) : size{ a.size } {
+		if (this != &a) {
+			// deep copy
+			if (data) delete[] data;
+			data = a.data;
+			a.data = nullptr;
+			a.size = 0;
+		}
+	}
+
+	~DynArray() {
+		delete[] data;
+	}
+
+	// copy operator
+	DynArray operator=(const DynArray& a)  {
+		if (this != &a) {
+			size = a.size;
+			if (data) delete[] data;
+			data = new int[size];
+			for (int k = 0; k < size; k++)
+				this->data[k] = a.data[k];
+			return *this;
+		}
+	}
+
+	// move operator
+	DynArray operator=(DynArray&& a) {
+		if (this != &a) {
+			size = a.size;
+			if (data) delete[] data;
+			data = a.data;
+			a.data = nullptr;
+			a.size = 0;
+			return *this;
+		}
+	}
+
+	void show() const {
+		for (int k = 0; k < size; k++)
+			cout << data[k] << " ";
+		cout << endl;
+	}
+
+	void set_data(int k, int val) {
+		data[k] = val;
+	}
+};
+
+void test_dynarray() {
+	int* data = new int[] {1, 2, 3, 4, 5, 6};
+	DynArray arr(6, data);
+	arr.show();
+	data[1] = 100;
+	arr.show();
+	
+	int* temp = new int[6]{};
+	DynArray arr2(6, temp);
+	arr2 = arr;
+	arr.set_data(1, 200);
+	cout << "arr: ";
+	arr.show();
+	cout << "arr2: ";
+	arr2.show();
+
+	arr2 = DynArray(6, data);
+	arr2.show();
+
+
+	delete[] data;
+	delete[] temp;
+}
+
+
 int main() {
-
-	Empty r();
-
-
+	test_dynarray();
+	//test_reference();
+	//Empty r();
 	//test1();
 	return 0;
 }
